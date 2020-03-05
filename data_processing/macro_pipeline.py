63003c8bb342318ae35f0b476e3469c56d21a777
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-03-05 18:03:53 +0100

    polishing of pipeline and macropipeline classes

--- data_processing/macro_pipeline.py
+++ data_processing/macro_pipeline.py
@@ -1,15 +1,40 @@
 import sys
-import dask.bag
-from dask.delayed import delayed
+
+# import dask
+# import dask.bag
+# from dask.delayed import delayed
+from dask.distributed import Client
 
 from pipeline import Pipeline
 
 
 class MacroPipeline(object):
+    """
+    Class to setup macro pipeline workflows. Each MacroPipeline object entails
+    multiple tasks that correspond to Pipeline instances. All the tasks can be
+    run in parallel using dask.
+
+    We implement dask for embarrassingly parallel tasks, see
+    https://examples.dask.org/applications/embarrassingly-parallel.html
+
+    Example:
+        >>> class Foo(Pipeline):
+        ...     def __init__(self):
+        ...         self.pipeline = ['task_info']
+        ...     def task_info(self):
+        ...         print(os.getpid())
+        >>> macro = MacroPipeline()
+        >>> one, two = Foo(), Foo()
+        >>> macro.tasks = [one, two]
+        >>> macro.run() # the default thread parallelism is used
+        61244
+        61245
+    """
     _tasks = None
 
     @property
     def tasks(self):
+        """ List of Pipeline instances that needs to be run. """
         if self._tasks is None:
             self._tasks = []
         return self._tasks
@@ -25,23 +50,37 @@ class MacroPipeline(object):
             raise TypeError('List is expected!')
 
     def add_task(self, task):
+        """
+        Add pipeline instance to the collection of tasks to be executed.
+
+        :param task: Pipeline instance.
+        """
         assert isinstance(task, Pipeline)
         self.tasks.append(task)
         return self
 
     @staticmethod
     def _run_task(f):
+        exctype, value = (None, None)
         try:
-            f()
-            exctype, value = (None, None)
+            _ = f()
         except:
             # sys.exc_info() provides info about current exception,
             # thus it must remain in the except block!
             exctype, value = sys.exc_info()[:2]
-        return [(exctype, value)]
+        return exctype, value
+
+    # def run(self):
+    #     """ Run the macro pipeline. """
+    #     delayed_tasks = [delayed(self._run_task)(task.run)
+    #                      for task in self.tasks]
+    #     # return dask.compute(*delayed_tasks)
+    #     bag = dask.bag.from_delayed(delayed_tasks)
+    #     return bag.compute()
 
     def run(self):
-        delayed_tasks = [delayed(self._run_task)(task.run)
-                         for task in self.tasks]
-        bag = dask.bag.from_delayed(delayed_tasks)
-        return bag.compute()
+        """ Run the macro pipeline. """
+        self.client = Client()  # TODO: find best way to setup client (set method?)
+        futures = [self.client.submit(self._run_task, task.run)
+                   for task in self.tasks]
+        return self.client.gather(futures)
--- data_processing/pipeline.py
+++ data_processing/pipeline.py
@@ -1,14 +1,34 @@
 from utils import get_args_from_configfile
 
-class Pipeline(object):
 
+class Pipeline(object):
+    """
+    Base Pipeline class to construct workflows. Inheriting classes should
+    define `pipeline` as the sequence of the methods that constitute the
+    pipeline. After storing the input of the various tasks in the `input`
+    dictionary, the pipeline can be run with the method `run`.
+
+    Example:
+         >>> class FooBar(Pipeline):
+         ...    def __init__(self):
+         ...        self.pipeline = ['foo', 'bar']
+         ...    def foo(self, a):
+         ...        print(a)
+         ...    def bar(self, b):
+         ...        print(b)
+         >>> test = FooBar()
+         >>> test.input = {'foo': {'a': 5}, 'bar': {'b': 6}}
+         >>> test.run()
+         5
+         6
+    """
     _pipeline = None
     _input = None
 
     @property
     def pipeline(self):
         """
-        List containing the consecutive steps that constitute the pipeline.
+        List containing the consecutive tasks that constitute the pipeline.
         """
         if self._pipeline is None:
             self._pipeline = []
@@ -16,10 +36,18 @@ class Pipeline(object):
 
     @pipeline.setter
     def pipeline(self, pipeline):
-        if isinstance(pipeline, list):
-            self._pipeline = pipeline
-        else:
-            raise TypeError('A list is expected!')
+        try:
+            tasks = iter(pipeline)
+        except TypeError:
+            print('The sequence of tasks in the pipeline '
+                  'should be provided as an iterable object.')
+            raise
+        for task in tasks:
+            if task not in dir(self.__class__):
+                raise AttributeError('Error defining the pipeline: {} method '
+                                     'not found in class {}'.format(task,
+                                                                    self.__class__.__name__))
+        self._pipeline = [s for s in pipeline]
 
     @property
     def input(self):
@@ -36,11 +64,12 @@ class Pipeline(object):
         if not isinstance(input, dict):
             raise TypeError("A dictionary is expected!")
         else:
+            # TODO: check whether all attributes are also in pipeline?
             self._input = input
 
     def config(self, path):
         """
-        Set the pipeline using input from a configfile.
+        Set the pipeline input by reading a configfile.
 
         :param path: Path to the configfile
         """
@@ -49,9 +78,21 @@ class Pipeline(object):
 
     def run(self):
         """ Run the full pipeline. """
-        for task_name in self.pipeline:
-            if task_name in self.input:
-                task = getattr(self, task_name)
-                task(**self.input[task_name])
-        return
+        _input = self.input.copy()
 
+        for task_name in self.pipeline:
+            if task_name in _input:
+                task = getattr(self, task_name)
+                input_task = _input.pop(task_name)
+                if isinstance(input_task, dict):
+                    task(**input_task)
+                elif (isinstance(input_task, list)
+                      or isinstance(input_task, tuple)):
+                    task(*input_task)
+                else:
+                    task(input_task)
+
+        if len(_input.keys()) > 0:
+            raise Warning('Some of the attributes in input have not been used:'
+                          ' {} '.format(', '.join(_input.keys())))
+        return
