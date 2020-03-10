c1441ae83995c37b511aa2968e25929ab79b2353
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-03-10 22:28:20 +0100

    tests added for macro_pipeline & utils

--- tests/test_macro_pipeline.py
+++ tests/test_macro_pipeline.py
@@ -1,10 +1,87 @@
+import os
+import shutil
 import unittest
 
+from lc_macro_pipeline.macro_pipeline import MacroPipeline
+from lc_macro_pipeline.pipeline import Pipeline
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+from tools import ShortIOPipeline
 
+class TestMacroPipelineObject(unittest.TestCase):
 
-if __name__ == '__main__':
-    unittest.main()
+    _tmp_dask_worker_dir = 'dask-worker-space'
+
+    def tearDown(self):
+        if os.path.isdir(self._tmp_dask_worker_dir):
+            shutil.rmtree(self._tmp_dask_worker_dir)
+
+    def test_tasksDefault(self):
+        mp = MacroPipeline()
+        self.assertTrue(isinstance(mp.tasks, list))
+        self.assertTrue(len(mp.tasks) == 0)
+
+    def test_setTasksNotValid(self):
+        mp = MacroPipeline()
+        pip = Pipeline()
+        with self.assertRaises(TypeError):
+            mp.tasks = 0
+        with self.assertRaises(TypeError):
+            mp.tasks = pip
+        with self.assertRaises(AssertionError):
+            mp.tasks = ['load']
+
+    def test_addTaskNotValid(self):
+        mp = MacroPipeline()
+        with self.assertRaises(AssertionError):
+            mp.add_task(0)
+        with self.assertRaises(AssertionError):
+            mp.add_task('load')
+        with self.assertRaises(AssertionError):
+            mp.add_task(['load'])
+
+class TestToyMacroPipeline(unittest.TestCase):
+
+    _test_dir = 'test_tmp_dir'
+    _tmp_dask_worker_dir = 'dask-worker-space'
+
+    def setUp(self):
+        os.mkdir(self._test_dir)
+
+    def tearDown(self):
+        shutil.rmtree(self._test_dir)
+        if os.path.isdir(self._tmp_dask_worker_dir):
+            shutil.rmtree(self._tmp_dask_worker_dir)
+
+    def test_runValidPipelines(self):
+        a, b = ShortIOPipeline(), ShortIOPipeline()
+        file_a, file_b = [os.path.join(self._test_dir, 'file_{}.txt'.format(s))
+                          for s in 'ab']
+        text = 'hello world'
+        a.input = {'open': file_a,
+                   'write': [text],
+                   'close': {}}
+        b.input = {'open': file_b,
+                   'write': [text],
+                   'close': {}}
+        mp = MacroPipeline()
+        mp.tasks = [a, b]
+        mp.run()
+        self.assertTrue(all([os.path.isfile(f) for f in [file_a, file_b]]))
+        lines_a, lines_b = [open(f).readlines() for f in [file_a, file_b]]
+        self.assertEqual(lines_a, lines_b)
+
+    def test_runInvalidPipeline(self):
+        a, b = ShortIOPipeline(), ShortIOPipeline()
+        file = os.path.join(self._test_dir, 'file_a.txt')
+        text = 'hello world'
+        a.input = {'open': file,
+                   'write': [text],
+                   'close': {}}
+        b.input = {'open': self._test_dir,
+                   'write': [text],
+                   'close': {}}
+        mp = MacroPipeline()
+        mp.tasks = [a, b]
+        errs = mp.run()
+        self.assertListEqual(list(errs[0]), [None, None])
+        self.assertTrue(errs[1][0], IsADirectoryError)
--- tests/tools.py
+++ tests/tools.py
@@ -1,3 +1,5 @@
+import os
+
 from lc_macro_pipeline.pipeline import Pipeline
 
 
@@ -14,4 +16,25 @@ class ShortPipeline(Pipeline):
     def bar(self, b, c=None):
         self.output['b'] = b
         self.output['c'] = c
+        return self
+
+
+class ShortIOPipeline(Pipeline):
+
+    def __init__(self):
+        self.pipeline = ['open', 'write', 'close']
+        self.fd = None
+
+    def open(self, path):
+        if os.path.isdir(path):
+            raise IsADirectoryError('path is dir')
+        self.fd = open(path, 'w')
+        return self
+
+    def write(self, text):
+        self.fd.write(text)
+        return self
+
+    def close(self):
+        self.fd.close()
         return self
