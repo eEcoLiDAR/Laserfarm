14c7db2fa7e2249401abc903f0db14e549febbbc
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-03-11 09:57:14 +0100

    Removed unnecessary string formatting in example

--- examples/example_macro_data_processing.py
+++ examples/example_macro_data_processing.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     for i in range(2):
         pipeline = DataProcessing()
-        pipeline.config('data_processing.json'.format(i))
+        pipeline.config('data_processing.json')
         macro.add_task(pipeline)
 
     res = macro.run()
--- examples/example_macro_retiler.py
+++ examples/example_macro_retiler.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     for i in range(2):
         pipeline = Retiler()
-        pipeline.config('retiling_config.json'.format(i))
+        pipeline.config('retiling_config.json')
         macro.add_task(pipeline)
 
     res = macro.run()
