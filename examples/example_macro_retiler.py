48170c281f678a238e9eff25db66336483af474a
rogerkuou <rogerkuou@gmail.com>
2020-04-01 09:43:27 +0200

    update testing setup for retiling

--- examples/example_macro_retiler.py
+++ examples/example_macro_retiler.py
@@ -1,25 +1,26 @@
-import sys
-sys.path.append('/mnt/c/Users/OuKu/Developments/eEcolidar/lcMacroPipeline')
-
 from lc_macro_pipeline.retiler import Retiler
 from lc_macro_pipeline.macro_pipeline import MacroPipeline
+
+mode_test='ssh' # 'local' or 'ssh'
 
 if __name__ == '__main__':
 
     macro = MacroPipeline()
-
     pipeline = Retiler()
-    pipeline.config('local_config/example_retiling_config.json') #local
-    # pipeline.config('cluster_config/example_retiling_config.json') # cluster
-    macro.add_task(pipeline)
-    macro.setup_client(mode='local') # local test
-    # macro.setup_client(mode = 'ssh',
-    #                    hosts = ["172.17.0.2", "172.17.0.3"], 
-    #                    connect_options={"known_hosts": None, 
-    #                                       "username":"ubuntu", 
-    #                                       "client_keys":"/home/ubuntu/.ssh/id_rsa"},
-    #                    worker_options={"nthreads": 1, "nprocs":2}, 
-    #                    scheduler_options={"dashboard_address": "8787"}) # cluster test
+    if mode_test == 'local':
+        pipeline.config('local_config/example_retiling_config.json')
+        macro.add_task(pipeline)
+        macro.setup_client(mode='local')
+    elif mode_test == 'ssh':
+        pipeline.config('cluster_config/example_retiling_config.json')
+        macro.add_task(pipeline)
+        macro.setup_client( mode = 'ssh',
+                            hosts = ["172.17.0.2", "172.17.0.2", "172.17.0.3"], 
+                            connect_options={"known_hosts": None, 
+                                                "username":"ubuntu", 
+                                                "client_keys":"/home/ubuntu/.ssh/id_rsa"},
+                            worker_options={"nthreads": 1, "nprocs":2}, 
+                            scheduler_options={"dashboard_address": "8787"})
     res = macro.run()
     print(res)
     macro.shutdown()
