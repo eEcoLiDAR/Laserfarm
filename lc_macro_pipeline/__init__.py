12d8ef5c9fcd1880321ed7c18c3076e11f7fe815
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-05-12 23:35:27 +0200

    fix setup.py: version not hardcoded and pytest dependency

--- lc_macro_pipeline/__init__.py
+++ lc_macro_pipeline/__init__.py
@@ -1,5 +1,8 @@
-__version__ = '0.1.0'
 __license__ = 'Apache Licence 2.0'
+__author__ = "Meiert Grootes, Ou Ku, Francesco Nattino"
+__email__ = 'team-atlas@esciencecenter.nl'
+
+from .__version__ import __version__
 
 from lc_macro_pipeline.data_processing import DataProcessing
 from lc_macro_pipeline.geotiff_writer import GeotiffWriter
