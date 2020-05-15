4e5786975ef2c031d6f62129336303fd061b6f54
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-05-15 15:59:58 +0200

    classification example updated

--- examples/example_macro_classification.py
+++ examples/example_macro_classification.py
@@ -1,26 +1,25 @@
-from laserfarm.classification import Classification
-from laserfarm.macro_pipeline import MacroPipeline
+from laserfarm import Classification, MacroPipeline
 
 mode_test = 'local'  # 'local' or 'ssh'
 
 if __name__ == '__main__':
 
     macro = MacroPipeline()
-    pipeline = Classification()
+    pipeline = Classification(input_file="tile_170_107.ply")
     if mode_test == 'local':
-        pipeline.config('local_config/example_classification.json')
+        pipeline.config(from_file='local_config/example_classification.json')
         macro.add_task(pipeline)
         macro.setup_cluster(mode='local')
     elif mode_test == 'ssh':
-        pipeline.config('cluster_config/example_classification.json')
+        pipeline.config(from_file='cluster_config/example_classification.json')
         macro.add_task(pipeline)
         macro.setup_cluster(mode='ssh',
-                            hosts=["172.17.0.2", "172.17.0.2", "172.17.0.3"],
+                            hosts=["172.17.0.2", "172.17.0.3"],
                             connect_options={"known_hosts": None,
                                              "username": "ubuntu",
                                              "client_keys": "/home/ubuntu/.ssh/id_rsa"},
                             worker_options={"nthreads": 1, "nprocs": 2},
                             scheduler_options={"dashboard_address": "8787"})
-    res = macro.run()
-    print(res)
+    macro.run()
+    macro.print_outcome()
     macro.shutdown()
